943b9f0a95b0ab9c6492d6328fe80a9923bd8e9a
Manuel Barkhau <mbarkhau@gmail.com>
2018-06-23 15:43:04 +0200

    stubs for some checkers

--- src/three2six/checkers.py
+++ src/three2six/checkers.py
@@ -27,9 +27,8 @@ class NoOverriddenBuiltinsChecker(CheckerBase):
         for node in ast.walk(tree):
             if isinstance(node, (ast.FunctionDef, ast.ClassDef)):
                 name_in_scope = node.name
-            elif isinstance(node, ast.Name):
-                if isinstance(node.ctx, ast.Store):
-                    name_in_scope = node.id
+            elif isinstance(node, ast.Name) and isinstance(node.ctx, ast.Store):
+                name_in_scope = node.id
             elif isinstance(node, ast.alias):
                 name_in_scope = node.name if node.asname is None else node.asname
             elif isinstance(node, ast.arg):
@@ -40,3 +39,40 @@ class NoOverriddenBuiltinsChecker(CheckerBase):
             if name_in_scope and name_in_scope in common.BUILTIN_NAMES:
                 # TODO (mb 2018-06-14): line numbers and file path
                 raise common.CheckError(f"Prohibited override of builtin '{name_in_scope}'")
+
+
+MODULE_BACKPORTS = {
+    "lzma"                : ((3, 3), "backports.lzma"),
+    "pathlib"             : ((3, 4), "pathlib2"),
+    "statistics"          : ((3, 4), "statistics"),
+    "ipaddress"           : ((3, 4), "py2-ipaddress"),
+    "asyncio"             : ((3, 4), None),
+    "selectors"           : ((3, 4), None),
+    "enum"                : ((3, 4), "enum34"),
+    "zipapp"              : ((3, 5), None),
+    "typing"              : ((3, 5), "typing"),
+    "contextvars"         : ((3, 7), "contextvars"),
+    "dataclasses"         : ((3, 7), "dataclasses"),
+    "importlib.resources" : ((3, 7), "importlib_resources"),
+}
+
+
+class NoThreeOnlyImports(CheckerBase):
+
+    def __call__(self, cfg: common.BuildConfig, tree: ast.Module):
+        pass
+
+
+class OnlyOpenWithRawModeChecker(CheckerBase):
+
+    def __call__(self, cfg: common.BuildConfig, tree: ast.Module):
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Call):
+                # print(node, dir(node))
+                pass
+
+
+class NoAsyncAwait(CheckerBase):
+
+    def __call__(self, cfg: common.BuildConfig, tree: ast.Module):
+        pass
