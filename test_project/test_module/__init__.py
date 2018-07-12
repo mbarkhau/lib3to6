3004e2de2bd6b8144553ea0e8d7be167f35f3074
Manuel Barkhau <mbarkhau@gmail.com>
2018-07-12 18:46:18 +0200

    update test project tests

--- test_project/test_module/__init__.py
+++ test_project/test_module/__init__.py
@@ -4,8 +4,30 @@
 #
 # SPDX-License-Identifier:    MIT
 
-who = "World"
 
-print(f"Hello {who}!")
+# test unpacking generalizations
 
-print("all ok")
+x = [*[1, 2], 3]
+(
+    lambda l: l.extend([*[1, 2, *l, 4], 5])
+)(x)
+assert x == [1, 2, 3, 1, 2, 1, 2, 3, 4, 5]
+
+
+def test_unpacking_generalization(*args, **kwargs):
+    return args, kwargs
+
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+x = {"x": 11}
+z = {"z": 33}
+
+args, kwargs = test_unpacking_generalization(0, *a, *b, 7, 8, **x, y=22, **z)
+assert args == (0, 1, 2, 3, 4, 5, 6, 7, 8)
+assert kwargs == {"x": 11, "y": 22, "z": 33}
+
+
+okfail = "ok"
+
+print(f"all {okfail}")
