afb15d2cdbe241c66d5a5258e528a57c70a8ad16
Manuel Barkhau <mbarkhau@gmail.com>
2018-07-12 18:45:18 +0200

    bump version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if "bdist_wheel" in sys.argv:
 
 setuptools.setup(
     name="three2six",
-    version="0.1.3",
+    version="0.2.0",
     description="Transpile modern python to universal python",
     long_description=read("README.rst"),
     long_description_content_type="text/x-rst",
