857eba5a6fe401f80ee643db924c5fc0c4dbf807
Manuel Barkhau <mbarkhau@gmail.com>
2019-02-22 22:03:12 +0100

    bump version to v201902.0028

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
     author="Manuel Barkhau",
     author_email="mbarkhau@gmail.com",
     url="https://gitlab.com/mbarkhau/lib3to6",
-    version="201902.27",
+    version="201902.28",
     keywords="six lib2to3 astor ast",
     description="Compile Python 3.6+ code to Python 2.7+",
     long_description=long_description,
