82ce4dc1572c690bded1375ac2e116ffc0909ec2
Manuel Barkhau <mbarkhau@gmail.com>
2020-02-22 01:25:49 +0000

    bump version to v202002.0031

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
     author="Manuel Barkhau",
     author_email="mbarkhau@gmail.com",
     url="https://gitlab.com/mbarkhau/lib3to6",
-    version="201902.30",
+    version="202002.31",
     keywords="six lib2to3 astor ast",
     description="Compile Python 3.6+ code to Python 2.7+",
     long_description=long_description,
--- src/lib3to6/__init__.py
+++ src/lib3to6/__init__.py
@@ -9,6 +9,6 @@ from .utils import parsedump_source
 from .packaging import fix
 from .transpile import transpile_module
 
-__version__ = "v201902.0030"
+__version__ = "v202002.0031"
 
 __all__ = ["fix", "transpile_module", "parsedump_ast", 'parsedump_source']
