bcb8dae21a8f3efe9560ac8e8bfdc293fa9e0c35
Manuel Barkhau <mb@admetrics.io>
2020-05-24 15:23:19 +0000

    bump version to v202005.1036-beta

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
     author="Manuel Barkhau",
     author_email="mbarkhau@gmail.com",
     url="https://gitlab.com/mbarkhau/lib3to6",
-    version="202005.1035b0",
+    version="202005.1036b0",
     keywords="six lib2to3 astor ast",
     description="Compile Python 3.6+ code to Python 2.7+",
     long_description=long_description,
