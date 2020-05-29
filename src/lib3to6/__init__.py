7649782e21988a1dbf9140d0ec98682ad087e5b7
Manuel Barkhau <mb@admetrics.io>
2020-05-29 22:34:44 +0000

    bump version to v202005.1040-beta

--- src/lib3to6/__init__.py
+++ src/lib3to6/__init__.py
@@ -9,6 +9,6 @@ from .utils import parsedump_source
 from .packaging import fix
 from .transpile import transpile_module
 
-__version__ = "v202005.1039-beta"
+__version__ = "v202005.1040-beta"
 
 __all__ = ["fix", "transpile_module", "parsedump_ast", 'parsedump_source']
