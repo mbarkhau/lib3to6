21893df62aac71f95f92ae11e1630cd955af1925
Manuel Barkhau <mbarkhau@gmail.com>
2021-08-12 11:05:05 +0000

    bump version to v202108.1048-b0

--- src/lib3to6/__init__.py
+++ src/lib3to6/__init__.py
@@ -10,6 +10,6 @@ from .packaging import fix
 from .packaging import build_py
 from .transpile import transpile_module
 
-__version__ = "v202107.1047"
+__version__ = "v202108.1048-b0"
 
 __all__ = ["fix", "transpile_module", "parsedump_ast", 'parsedump_source', 'build_py']
